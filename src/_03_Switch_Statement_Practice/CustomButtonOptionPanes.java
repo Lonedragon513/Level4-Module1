2069044c85dade41459f236da2df303945e976bf
lonedragon513 <lonedragon513@192.168.3.122>
2020-08-22 17:28:20 -0700

    f

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -12,6 +12,52 @@ public class CustomButtonOptionPanes {
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
+		switch(choice) {
+		case "Sunday":
+			System.out.println("Sunday");
+			break;
+		case "Monday":
+			System.out.println("Monday");
+			break;
+		case "Tuesday":
+			System.out.println("Tuesday");
+			break;
+		case "Wednesday":
+			System.out.println("Wednesday");
+			break;
+		case "Thursday":
+			System.out.println("Thursday");
+			break;
+		case "Friday":
+			System.out.println("Friday");
+			break;
+		case "Saturday":
+			System.out.println("Saturday");
+			break;
+		}
+//		switch(input) {
+//		case 0:
+//			System.out.println("Sunday");
+//			break;
+//		case 1:
+//			System.out.println("Monday");
+//			break;
+//		case 2:
+//			System.out.println("Tuesday");
+//			break;
+//		case 3:
+//			System.out.println("Wednesday");
+//			break;
+//		case 4:
+//			System.out.println("Thursday");
+//			break;
+//		case 5:
+//			System.out.println("Friday");
+//			break;
+//		case 6:
+//			System.out.println("Saturday");
+//			break;
+//		}
 		
 	}
 }
--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -5,10 +5,76 @@ import javax.swing.JOptionPane;
 public class _00_Horoscope {
 	// 1. Create an enum in a separate file called Zodiac that contains a category for
 	//    all 12 zodiac signs.
+	public enum Zodiac {
+		ARIES,TAURUS,GEMINI,CANCER,LEO,VIRGO,LIBRA,SCORPIO,SAGITTARIUS,CAPRICORN,AQUARIUS,PISCES
+	}
 	
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
-	
+	void metho (Zodiac a ){
+		switch (a) {
+		case ARIES: {
+			System.out.println("sheep");
+			break;
+		}
+		case AQUARIUS: {
+			System.out.println("water");
+			break;
+			}
+		case GEMINI: {
+			System.out.println("twin(?)");
+			break;
+			}
+		case CANCER: {
+			System.out.println("crab");
+			break;
+			
+			}
+		case LEO: {
+			System.out.println("lion");
+			break;
+			
+			}
+		case VIRGO: {
+			System.out.println("grass(?)");
+			break;
+			
+			}
+		case LIBRA: {
+			System.out.println("scale");
+			break;
+			
+			}
+		case SCORPIO: {
+			System.out.println("scorpion");
+			break;
+			
+			}
+		case SAGITTARIUS: {
+			System.out.println("horse");
+			break;
+			
+			}
+		case CAPRICORN: {
+			System.out.println("goat(?)");
+			break;
+			
+			}
+		case PISCES: {
+			System.out.println("fish");
+			break;
+			
+			}
+		case TAURUS: {
+			System.out.println("bull");
+			break;
+			}
+		}
+		}
+		
 	// 3. Make a main method to test your method
+		public static void main(String[] args) {
+
+		}
 	
 }
